e1942ff072dd86eb9b2fe7affe52d8b3a9176505
Yevhen Kaskov <kaskov.e@gmail.com>
2023-03-29 13:44:07 +0300

    Created models

--- newspaper/models.py
+++ newspaper/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    # TODO: ordering by date_published
+    # class Meta:
+    #     ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} {self.country}"
+
+
+class Redactor(AbstractUser):
+    # username = models.CharField(max_length=255, unique=True)
+    years_of_experience = models.IntegerField(max_length=63)
+
+    class Meta:
+        verbose_name = "redactor"
+        verbose_name_plural = "publishers"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    # def get_absolute_url(self):
+    #     return reverse("taxi:driver-detail", kwargs={"pk": self.pk})
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    publishers = models.ManyToManyField(Redactor, related_name="newspapers")
+
+    def __str__(self):
+        return self.title
