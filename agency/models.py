79b97b9767d1125b10bff232838aaaa08c4a94f5
Yevhen Kaskov <kaskov.e@gmail.com>
2023-04-01 17:40:06 +0300

    commit before deploying

--- agency/models.py
+++ agency/models.py
@@ -11,13 +11,11 @@ class Topic(models.Model):
 
 
 class Redactor(AbstractUser):
-    # username = models.CharField(max_length=255, unique=True)
     years_of_experience = models.IntegerField(blank=True, null=True)
 
     class Meta:
         verbose_name = "redactor"
         verbose_name_plural = "redactors"
-        # verbose_name_plural = "publishers"
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
