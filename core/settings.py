bf242ee476c89a911c83814d8aa5c72b964387e5
inrock1 <kaskov.e@gmail.com>
2023-08-26 01:05:16 +0300

    debug false and COOKIE_SECURE true

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+# DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = False
 
 if DEBUG:
     print("Debug mode on")
@@ -48,12 +49,12 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-# if DEBUG:
-#     CSRF_COOKIE_SECURE = False
-#     SESSION_COOKIE_SECURE = False
-# else:
-#     CSRF_COOKIE_SECURE = True
-#     SESSION_COOKIE_SECURE = True
+if DEBUG:
+    CSRF_COOKIE_SECURE = False
+    SESSION_COOKIE_SECURE = False
+else:
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
 
 
 # Application definition
